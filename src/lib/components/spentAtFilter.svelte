50a0b06d6652e0bc51a74565d480615ddc0ca1b7
rogo.alit <rogo.alit@tech.jago.com>
2025-03-05 12:05:26 +0700

    feat: add new date range filter

--- src/lib/components/spentAtFilter.svelte
+++ src/lib/components/spentAtFilter.svelte
@@ -1,28 +1,43 @@
 <script lang="ts">
   import type { SpendingCreatedAtRange } from "$lib/interfaces";
   import dayjs from "dayjs";
-  import { onMount } from "svelte";
+
+  const todayEndOfDayDateString = dayjs().endOf("day").toISOString();
 
   const dateRangeNameToDateValueMap = new Map<string, SpendingCreatedAtRange>([
     [
       "Today",
       {
         fromInclusive: dayjs().startOf("day").toISOString(),
-        toExclusive: dayjs().endOf("day").toISOString(),
+        toExclusive: todayEndOfDayDateString,
       },
     ],
     [
       "Last 3 Days",
       {
         fromInclusive: dayjs().subtract(3, "day").startOf("day").toISOString(),
-        toExclusive: dayjs().endOf("day").toISOString(),
+        toExclusive: todayEndOfDayDateString,
       },
     ],
     [
       "Last 7 Days",
       {
         fromInclusive: dayjs().subtract(7, "day").startOf("day").toISOString(),
-        toExclusive: dayjs().endOf("day").toISOString(),
+        toExclusive: todayEndOfDayDateString,
+      },
+    ],
+    [
+      "This Week",
+      {
+        fromInclusive: dayjs().startOf("week").startOf("day").toISOString(),
+        toExclusive: todayEndOfDayDateString,
+      },
+    ],
+    [
+      "This Month",
+      {
+        fromInclusive: dayjs().startOf("month").startOf("day").toISOString(),
+        toExclusive: todayEndOfDayDateString,
       },
     ],
   ]);
