22904e6f431c23a50fca9616e506ff332c5ae3bf
rogojagad <rogojagadalit@gmail.com>
2026-09-14 13:01:14 +0700

    feat: update date range calculations to use consistent date formatting

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,46 +2,48 @@ import dayjs from "dayjs";
 import type { DateRange } from "./interfaces";
 
 const todayEndOfDayDateString = dayjs().endOf("day").toISOString();
+const dateRangeValueFormat = "YYYY-MM-DD"
+
 export const dateRangeNameToDateValueMap = new Map<string, DateRange>([
   [
     "Today",
     {
-      fromInclusive: dayjs().startOf("day").toISOString(),
+      fromInclusive: dayjs().format(dateRangeValueFormat),
       toExclusive: todayEndOfDayDateString,
     },
   ],
   [
     "Yesterday",
     {
-      fromInclusive: dayjs().subtract(1, "day").startOf("day").toISOString(),
+      fromInclusive: dayjs().subtract(1, "day").format(dateRangeValueFormat),
       toExclusive: dayjs().subtract(1, "day").endOf("day").toISOString(),
     },
   ],
   [
     "Last 3 Days",
     {
-      fromInclusive: dayjs().subtract(2, "day").startOf("day").toISOString(),
+      fromInclusive: dayjs().subtract(2, "day").format(dateRangeValueFormat),
       toExclusive: todayEndOfDayDateString,
     },
   ],
   [
     "Last 7 Days",
     {
-      fromInclusive: dayjs().subtract(6, "day").startOf("day").toISOString(),
+      fromInclusive: dayjs().subtract(6, "day").format(dateRangeValueFormat),
       toExclusive: todayEndOfDayDateString,
     },
   ],
   [
     "This Week",
     {
-      fromInclusive: dayjs().startOf("week").startOf("day").toISOString(),
+      fromInclusive: dayjs().startOf("week").format(dateRangeValueFormat),
       toExclusive: todayEndOfDayDateString,
     },
   ],
   [
     "This Month",
     {
-      fromInclusive: dayjs().startOf("month").startOf("day").toISOString(),
+      fromInclusive: dayjs().startOf("month").format(dateRangeValueFormat),
       toExclusive: todayEndOfDayDateString,
     },
   ],
